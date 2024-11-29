86bb715b16686d9920903122a56be46a99a83d2e
NatKond <natzubova@gmail.com>
2024-11-29 14:37:08 +0100

    Add file myfirstbashscript.sh

---
+++ myfirstbashscript.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+USER="Natalia Kondratenko"
+VAR=2
+date
+echo Hello $USER!
+pwd
+ps -ef | tail -n +2 | wc -l
+ps -ef | grep bioset | grep -v grep | wc -l
+ls -la /etc/passwd | awk '{print $1}'
